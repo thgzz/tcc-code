27f91315d124d2513e2cb64c97694007656b653c
Thiago Costa <thgzcr@gmail.com>
2023-10-25 18:59:26 -0300

    bioheat

--- ftcs_final.m
+++ ftcs_final.m
@@ -5,12 +5,20 @@ Ly = 1;         % Length of the domain in the y-direction
 Nx = 50;        % Number of grid points in the x-direction
 Ny = 50;        % Number of grid points in the y-direction
 Nt = 500;
-T = 1;        % Total simulation time
-alpha = 0.01;   % Thermal diffusivity
+T = 0.1;        % Total simulation time
 dx = Lx / (Nx - 1);
 dy = Ly / (Ny - 1);
-% dt = T / Nt;
-dt = 0.01;      % Time step size
+dt = 0.001;      % Time step size
+
+% alpha = 0.01;   % Thermal diffusivity
+k = 0.54;       % Thermal conductivity
+rho = 1066;
+c = 3763;
+alpha = k / ( rho * c);
+w_blood = 0.002; % Blood perfusion rate
+C_blood = 3617; % Specific heat of blood
+rho_b = 1050;
+
 
 % Create grid
 x = linspace(0, Lx, Nx);
@@ -31,8 +39,7 @@ source_amplitude = 10;
 source_center_x = Lx / 2;
 source_center_y = Ly / 2;
 source_radius = 0.2;   % Radius of influence
-source = @(x, y, t) source_amplitude + source_rate * current_time * ...
-                      (1 - exp(-((x - source_center_x).^2 + (y - source_center_y).^2) / (2*source_radius^2)));
+source = @(x, y, t) 1 * (1 - exp(-((x - source_center_x).^2 + (y - source_center_y).^2) / (2*source_radius^2)));
 
 
 
@@ -51,9 +58,9 @@ for t = 0:dt:T
     % Update interior points using the FTCS method with the source term
     for i = 2:Nx-1
         for j = 2:Ny-1
-            % FTCS scheme for 2D heat equation with source term
+            % Pennes' equation with source term
             u_new(i, j) = u(i, j) + alpha * dt * ((u(i+1, j) - 2*u(i, j) + u(i-1, j)) / dx^2 + ...
-                (u(i, j+1) - 2*u(i, j) + u(i, j-1)) / dy^2) + dt * source(x(i), y(j), t);
+                (u(i, j+1) - 2*u(i, j) + u(i, j-1)) / dy^2) + dt * (source(x(i), y(j), t) - w_blood * (u(i, j) - 37) * C_blood * rho_b);
         end
     end
     
@@ -61,15 +68,15 @@ for t = 0:dt:T
     u = u_new;
     
     % Visualization (optional)
-    if mod(t, 0.01) == 0  % Plot every 0.01 seconds
-        contourf(X, Y, u, 20, 'EdgeColor', 'none');
-        colorbar;
-        axis square;
-        title(['Time: ' num2str(t)]);
-        xlabel('X');
-        ylabel('Y');
-        drawnow;
-    end
+    % if mod(t, 0.01) == 0  % Plot every 0.01 seconds
+    %     contourf(X, Y, u, 20, 'EdgeColor', 'none');
+    %     colorbar;
+    %     axis square;
+    %     title(['Time: ' num2str(t)]);
+    %     xlabel('X');
+    %     ylabel('Y');
+    %     drawnow;
+    % end
 end
 
 % Final temperature distribution
