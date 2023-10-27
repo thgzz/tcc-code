0520a8bf7d70a68ccd95f9bc54cf5faa9eb89430
Thiago Costa <thgzcr@gmail.com>
2023-10-27 14:08:22 -0300

    versao final

--- ftcs_final.m
+++ ftcs_final.m
@@ -1,10 +1,10 @@
 clc; clear; close all
 % Parameters
-Lx = 150;         % Length of the domain in the x-direction
-Ly = 150;         % Length of the domain in the y-direction
-Nx = 5000;        % Number of grid points in the x-direction
+Lx = 0.15;         % Length of the domain in the x-direction
+Ly = 0.15;         % Length of the domain in the y-direction
+Nx = 500;        % Number of grid points in the x-direction
 Ny = 500;        % Number of grid points in the y-direction
-T = 300;        % Total simulation time
+T = 60;        % Total simulation time
 dx = Lx / (Nx - 1);
 dy = Ly / (Ny - 1);
 
@@ -33,9 +33,9 @@ u = 37 * ones(Nx, Ny);
 u_new = u;
 
 % Define a source term function (e.g., a Gaussian source)
-r = 75;
-r0 = 10;
-A = 0.17e6;
+r = Lx / 2;
+r0 = 0.01;
+A = 1.3e6;
 % y moves in x and x moves in y
 source_center_x = Lx / 2;
 source_center_y = Ly / 2;
@@ -45,11 +45,11 @@ source_center_y = Ly / 2;
 source = @(x, y, t) A * exp( -( (x - r).^2 + (y - source_center_y).^2 ) / r0^2);
 
     % Apply boundary conditions
-    u_new(1, :) = 37;  % Dirichlet boundary condition on the left
-    u_new(:, 1) = 37;  % Dirichlet boundary condition on the bottom
-    u_new(end, :) = 37;  % Dirichlet boundary condition on the right
+    u_new(1, :) = 37;  % Dirichlet boundary condition on the left (top)
+    u_new(:, 1) = 37;  % Dirichlet boundary condition on the bottom (left)
+    u_new(end, :) = 37;  % Dirichlet boundary condition on the right (bottom)
     
-    % Neumann boundary condition on the top (zero gradient)
+    % Neumann boundary condition on the top (zero gradient) (right)
     u_new(:, end) = u_new(:, end-1); 
 
 
@@ -64,7 +64,7 @@ for t = 0:dt:T
             source_term = source(x(i), y(j), t);
             % Pennes' equation with source term
             u_new(i, j) = u(i, j) + alpha * dt * ((u(i+1, j) - 2*u(i, j) + u(i-1, j)) / dx^2 + ...
-                (u(i, j+1) - 2*u(i, j) + u(i, j-1)) / dy^2) + dt * (source_term - w_blood * (u(i, j) - 37) * C_blood * rho_b);
+                (u(i, j+1) - 2*u(i, j) + u(i, j-1)) / dy^2) + dt * ((source_term - w_blood * (u(i, j) - 37) * C_blood * rho_b) / ( c * rho )); 
         end
     end
     
@@ -72,7 +72,7 @@ for t = 0:dt:T
     u = u_new;
     
     % Visualization (optional)
-    % if mod(t, 0.01) == 0  % Plot every 0.01 seconds
+    % if mod(t, 0.1) == 0  % Plot every 0.01 seconds
     %     contourf(X, Y, u, 20, 'EdgeColor', 'none');
     %     colorbar;
     %     axis square;
@@ -101,3 +101,12 @@ axis square;
 title('Final Temperature Distribution');
 xlabel('X');
 ylabel('Y');
+
+% Define the circle parameters
+% circle_radius = 0.05;
+% circle_center_x = Lx / 2;
+% circle_center_y = Ly / 2;
+
+% % Draw the circle
+% rectangle('Position', [circle_center_x - circle_radius, circle_center_y - circle_radius, 2*circle_radius, 2*circle_radius], ...
+%     'Curvature', [1, 1], 'EdgeColor', 'r', 'LineWidth', 2);
