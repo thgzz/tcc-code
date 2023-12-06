e3b49decb96be97761f18b3ff0d65e1853f64a2b
Thiago Costa <thgzcr@gmail.com>
2023-12-06 18:09:47 -0300

    new file for no nano

--- post_nonano.m
+++ post_nonano.m
@@ -1,35 +1,30 @@
 clc; clear; close all;
 
-
-load dados_nano.mat
-
-% vetor tempo 300s
-tp = linspace(0,T,500);
+load dados.mat
 
 Tm = u;
 l = length(Tm);
-% [T_max, idx] = max(Tm,[],"all")
-T_max = max(max(Tm))
-[Tmax_idxx, Tmax_idxy] = find(Tm==T_max)
+T_max = max(max(Tm));
+[Tmax_idxx, Tmax_idxy] = find(Tm==T_max);
 
 T_x = Tm(250,:);
 
 [~,TT] = find(T_x>41,1);
 
 [~, ridx] = min(abs(x-0.055));
+[~, ridy] = min(abs(y-0.055));
 
-T184 = Tm(250,184);
+x_163 = x(163);
+x_184 = x(184);
+xHealthy = round(x_184 - x_163,3);
 
-r_187 = round(x(187),3)
-R = 2;
-r = 1.9;
-anlA = pi*(R^2-r^2)
-
-AR = 12.57;
-Ar = 11.34;
-
-rDiff = 100*(AR-Ar)/Ar
-
+T184 = Tm(250,163)
+r = 2;
+R = 2.6;
+areaSaudavel = pi*(R^2-r^2);
+Ar = 12.57;
+AR = 21.24;
+rDiff = 100*(Ar-AR)/AR
 
 % hfig = figure;
 % plot(x,T_x,'LineWidth',1.5)
@@ -39,7 +34,7 @@ rDiff = 100*(AR-Ar)/Ar
 % xline(5.5/100,'k--','Limite do tumor','LineWidth',1)
 % xline(9.5/100,'k--','Limite do tumor','LineWidth',1)
 % yline(41,'r--','Necrose celular')
-% fname2 = 'linegraph_nano';
+% fname2 = 'linegraph_nonano';
 
 % picturewidth = 20; % set this parameter and keep it forever
 % hw_ratio = 0.65; % feel free to play with this ratio
