eb619cd494d3f81c242eff5742239eabcda3e9a8
Thiago Costa <thgzcr@gmail.com>
2023-12-09 00:39:08 -0300

    dunno change

--- post_nanor11.m
+++ post_nanor11.m
@@ -15,13 +15,14 @@ Txx = Tg(250,:);
 rr = 2;
 x182 = x(182)
 x184 = x(184)
-x250 = round(x(250),3)
-xfinal = (x184-x182)*100
-xx = (x250*100)+(rr+xfinal)
-ff = round(xx*100,2)
+x250 = round(x(250),3);
+xfinal = (x184-x182)*100;
+xx = (x250*100)+(rr+xfinal);
+ff = round(xx*100,2);
 
 [~, ridx] = min(abs(x-0.055));
 
 T184 = Tg(250,184)
+T182 = Tg(250,182)
 
 r_187 = round(x(187),3)
